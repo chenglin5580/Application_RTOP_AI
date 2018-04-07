80104001067f0da10e8c23dd75c6e3e8e3948280
cheng <chenglin5580@163.com>
2018-04-07 09:39:03 +0800

    2018.04.07 交叉验证gamma=1，但是终端精度还不满意

--- 2 QUAD/1 A3C/main_A3C.py
+++ 2 QUAD/1 A3C/main_A3C.py
@@ -5,14 +5,14 @@ from QUAD import QUAD as Objective_AI
 
 env = Objective_AI(random=False)
 train_flag = True
-# train_flag = False
+train_flag = False
 para = A3C.Para(env,  # 环境参数包括state_dim,action_dim,abound,step,reset
                 a_constant=True,  # 动作是否是连续
                 units_a=300,  # 双层网络，第一层的大小
                 units_c=300,  # 双层网络，critic第一层的大小
                 MAX_GLOBAL_EP=10000,  # 全局需要跑多少轮数
                 UPDATE_GLOBAL_ITER=200,  # 多少代进行一次学习，调小一些学的比较快
-                gamma=0.9,  # 奖励衰减率
+                gamma=1,  # 奖励衰减率
                 ENTROPY_BETA=0.01,  # 表征探索大小的量，越大结果越不确定
                 LR_A=0.00001,  # Actor的学习率
                 LR_C=0.001,  # Crtic的学习率
